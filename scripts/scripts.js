32277f4ef2682c8b9f88be1506f008953f6b00ea
khaled-bissani <khaled.w.bissani@gmail.com>
2022-09-10 18:32:29 +0300

    Added the gender and age api in js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -10,5 +10,30 @@ const newNationality = document.getElementById('nationality-id');
 generateBtn.addEventListener('click', generateAllValues);
 // Generate values function
 function generateAllValues() {
-    console.log("Test");
+    // Declare the value of the text input
+    const nameField = document.getElementById('name').value;
+    // Declare gender's url
+    var urlGender = 'https://api.genderize.io?name=' + nameField;
+    // Declare age's url
+    var urlAge = 'https://api.agify.io/?name=' + nameField
+
+    if (nameField == 0) {
+        console.log('error');
+    }
+    else {
+        // Fetch gender
+        fetch(urlGender)
+        .then(res => res.json())
+        .then(data => {
+            newGender.innerHTML = `<p>${data.gender}</p> `
+        });
+        // Fetch age
+        fetch(urlAge)
+		.then(res => res.json())
+		.then(data => {
+			newAge.innerHTML = `<p>${data.age}</p> `
+		});
+    }
+        
+
 }
