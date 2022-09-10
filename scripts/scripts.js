de86334447908f32b509728be7207e09797214f0
khaled-bissani <khaled.w.bissani@gmail.com>
2022-09-10 15:30:37 +0300

    Set up the main function in js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,7 +1,14 @@
 // Declare the generate button
-const generateBtn = document.getElementById('generate-id');
+const generateBtn = document.getElementById('generate-btn');
 // Declare all the placeholders
 const dogPicture = document.getElementById('dog-picture');
 const newGender = document.getElementById('gender-id');
 const newAge = document.getElementById('age-id');
 const newNationality = document.getElementById('nationality-id');
+
+// Generate button event
+generateBtn.addEventListener('click', generateAllValues);
+// Generate values function
+function generateAllValues() {
+    console.log("Test");
+}
