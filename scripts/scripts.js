39eae479f8449461cd563d86fee55909421a088d
khaled-bissani <khaled.w.bissani@gmail.com>
2022-09-10 15:18:33 +0300

    Declared some variables in js

---
+++ scripts/scripts.js
@@ -0,0 +1,7 @@
+// Declare the generate button
+const generateBtn = document.getElementById('generate-id');
+// Declare all the placeholders
+const dogPicture = document.getElementById('dog-picture');
+const newGender = document.getElementById('gender-id');
+const newAge = document.getElementById('age-id');
+const newNationality = document.getElementById('nationality-id');
